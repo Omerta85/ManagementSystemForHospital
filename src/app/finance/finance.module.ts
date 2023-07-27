0257da1777c023ac47c2720089d7d13767afd4de
Omerta85 <666Omerta@gmail.com>
2023-07-27 18:13:05 +0300

    add finance page

--- src/app/finance/finance.module.ts
+++ src/app/finance/finance.module.ts
@@ -1,33 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { PatientRoutingModule } from './patient-routing.module';
-import { PatientComponent } from './patient.component';
+import { FinanceRoutingModule } from './finance-routing.module';
+import { FinanceComponent } from './finance.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { HomeComponent } from './home/home.component';
-import { AppointmentComponent } from './appointment/appointment.component';
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
-import {ChartModule} from "angular-highcharts";
 import {NgxPaginationModule} from "ngx-pagination";
 
 
 @NgModule({
   declarations: [
-    PatientComponent,
+    FinanceComponent,
     SidenavComponent,
-    HomeComponent,
-    AppointmentComponent,
-  ],
-  exports: [
-    HomeComponent,
-    SidenavComponent
+    HomeComponent
   ],
   imports: [
     CommonModule,
-    PatientRoutingModule,
+    FinanceRoutingModule,
     FontAwesomeModule,
-    ChartModule,
     NgxPaginationModule
   ]
 })
-export class PatientModule { }
+export class FinanceModule { }
